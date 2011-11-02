658e7fa0b61304201d329da2ff18c940092109b3
Lance Cooper <lwcooper@gmail.com>
2011-11-02 09:09:13 +0000

    conversion to jQuery plugin

--- jquery.accordian.js
+++ jquery.accordian.js
@@ -1,74 +1,88 @@
-var accordian = {
-
-  init : function(el) {
-  
-    //Variables
-    accordian.activeClass = "active";
-    accordian.accHeaderClass = "accHeader";
-    accordian.accContentClass = "accContent";
-  
-    //Cache selectors
-    accordian.accHeader = el.find("." + accordian.accHeaderClass);
-    accordian.accContent = el.find("." + accordian.accContentClass);
-  
-    //Hide all our content    
-    accordian.accContent.hide();
-    
-    //Attach click handlers
-    accordian.accHeader.click(function() {
-      
-      //Cache selectors
-      $el = $(this).parent();
+(function ($) {
+    $.accordian = function (el, options) {
+        // To avoid scope issues, use 'base' instead of 'this'
+        // to reference this class from internal events and functions.
+        var base = this;
         
-      //Open this accordian
-      accordian.open($el);
-      
-      //Close accordian
-      //accordian.close();
-      
-    });
-  
-  },
-  
-  open : function(el) {
-    
-    //If current element is active we need to close it
-    if (el.hasClass(accordian.activeClass)) {
-      
-      accordian.close(el);
-      
-    } else {
-      
-      //If there is active content
-      if(accordian.active) {
+        // Access to jQuery and DOM versions of element
+        base.$el = $(el);
+        base.el = el;
         
-        //Close the active content
-        accordian.close(accordian.active);
+        // Add a reverse reference to the DOM object
+        base.$el.data("accordian", base);
         
-      }
+        base.init = function () {
+            base.options = $.extend({}, $.accordian.defaultOptions, options);
+            
+            //Store our active class (remove the first character)
+            base.active = $.accordian.defaultOptions.active.substring(1);
+            
+            //Hide all of our content
+            base.$el.find($.accordian.defaultOptions.content).hide();
+            
+            //Attach event listeners            
+            base.$el.find($.accordian.defaultOptions.header).click(function () {
+              
+                //Open the accordian
+                base.open($(this).parent());
+              
+            });
+        };
+        
+        base.open = function (el) {
+          
+            //If current element is active we need to close it
+            if (el.hasClass(base.active)) {
+              
+                base.close(el);
+              
+            } else {
+                    
+                //If there is active content
+                if (base.$active) {
+                  
+                    //Close the active content
+                    base.close(base.$active);
+                  
+                }
+                
+                //Store the active element
+                base.$active = el;
+                
+                //Add a classname for styling
+                el.addClass(base.active);
+                
+                //Display the content
+                el.find($.accordian.defaultOptions.content).slideDown("fast");
+              
+            }
+          
+        };
+        
+        base.close = function (el) {
+          
+            //Remove active class
+            el.removeClass(base.active);
+            
+            //Hide content
+            el.find($.accordian.defaultOptions.content).slideUp("fast");
+       
+        };
+        
+        // Run initializer
+        base.init();
+    };
     
-      //Store the active element
-      accordian.active = el;
-      
-      //Add a classname for styling
-      el.addClass(accordian.activeClass);
-      
-      //Display the content
-      el.find("." + accordian.accContentClass).slideDown("fast");
+    $.accordian.defaultOptions = {
+        header: ".accHeader",
+        content: ".accContent",
+        active: ".active"
+    };
     
-    }
+    $.fn.accordian = function (options) {
+        return this.each(function () {
+            (new $.accordian(this, options));
+        });
+    };
     
-    
-  },
-  
-  close : function(el) {
-  
-    //Remove active class
-    el.removeClass(accordian.activeClass);
-    
-    //Hide content
-    el.find("." + accordian.accContentClass).slideUp("fast");
-  
-  }
-
-}
+}(jQuery));
